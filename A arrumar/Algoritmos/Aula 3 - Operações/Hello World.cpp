125a1b4d4857a1f5f87abd9ca597866e95e5b194
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-07-09 21:32:30 -0300

    remoção de alguns arquivos

--- A arrumar/Algoritmos/Aula 3 - Operações/Hello World.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-//meu primeiro programa
-main(){
-	printf("Hello world!!!\n\n");
-	system("pause");
-}
--- A arrumar/Algoritmos/Aula 4 - Char/string.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main(){
-	char nome[20];
-	printf("\t\"Nome\": ");
-	//scanf("%s",&nome);
-	gets(nome);
-	printf("Nome digitado: %s\n",nome);
-	printf("Pos. 3: %i\n",nome[3]);
-}
